4dfecf64d7412797e65d1686b4c73517a646b0c8
Erlend <erlendbasso@gmail.com>
2021-02-16 17:11:05 +0100

    Update set_parameters.m

--- ROS/matlab/set_parameters.m
+++ ROS/matlab/set_parameters.m
@@ -3,14 +3,27 @@
 % 
 % rosinit('192.168.1.2')
 ptree = rosparam;
-pval.Kp = 0.3;
 pval.Kpy = 0.3;
 pval.Kppsi = 0.3;
 pval.Kd = 0.5;
 pval.ref.x = 0.2;
 pval.ref.y = 0.3;
 pval.ref.psi = pi/4;
+
+pval.gains.L11 = 10;
+pval.gains.L12 = 10;
+pval.gains.L13 = 10;
+
+pval.gains.L21 = 100;
+pval.gains.L22 = 100;
+pval.gains.L23 = 100;
+
+pval.gains.L31 = 10;
+pval.gains.L32 = 10;
+pval.gains.L33 = 10;
+
+
 set(ptree,'CSEI',pval)
 
 % Inspect parameters by using the get() function
-% get(ptree,'my_params')
+% get(ptree,'my_params') /CSEI/gains/L12
