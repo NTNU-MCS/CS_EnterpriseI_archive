a659c514ad9750f7ae7268a020875f16d4bab87f
Erlend <erlendbasso@gmail.com>
2021-02-23 18:50:48 +0100

    Case study C

--- ROS/matlab/publish_gains.m
+++ ROS/matlab/publish_gains.m
@@ -4,7 +4,6 @@ rosinit('192.168.1.2')
 
 [pub_obs,obs_msg] = rospublisher('CSEI/observer/gains','std_msgs/Float64MultiArray');
 [pub_ctrl,ctrl_msg] = rospublisher('CSEI/controller/gains','std_msgs/Float64MultiArray');
-[pub_speed,speed_msg] = rospublisher('CSEI/speed_ref','std_msgs/Float64');
 
 
 
@@ -13,9 +12,7 @@ rosinit('192.168.1.2')
 
 obs_msg.Data =[1.0 2.0 3.0]';
 ctrl_msg.Data = [2.0 4.0 13.37 420.0]';
-speed_msg.Data = 2;
 
 
 send(pub_ctrl, ctrl_msg);
 send(pub_obs,obs_msg);
-send(pub_speed, speed_msg);
