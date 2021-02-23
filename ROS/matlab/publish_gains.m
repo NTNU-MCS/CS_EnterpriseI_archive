3c6eeaddddb3b6cb967fe1ed97d21b7df826c4a4
Erlend <erlendbasso@gmail.com>
2021-02-23 16:34:11 +0100

    Added publish_gains.m

---
+++ ROS/matlab/publish_gains.m
@@ -0,0 +1,17 @@
+%% Run this block ONCE
+
+rosinit('192.168.1.2')
+
+[pub_obs,obs_msg] = rospublisher('CSEI/observer/gainss','std_msgs/Float64MultiArray');
+[pub_ctrl,ctrl_msg] = rospublisher('CSEI/controller/gainss','std_msgs/Float64MultiArray');
+
+
+
+%% Run this block every time you want to publish new data for the controller and/or observer gains.
+
+
+obs_msg.Data =[1.0 2.0 3.0]';
+ctrl_msg.Data = [2.0 4.0 13.37 420.0]';
+
+send(pub_ctrl, ctrl_msg);
+send(pub_obs,obs_msg);
